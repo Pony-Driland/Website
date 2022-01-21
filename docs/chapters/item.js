a1dca6aae7c9c17784ef3a7e5ea8c69a0cf15ec5
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-21 12:33:43 -0300

    new loader

--- docs/chapters/item.js
+++ docs/chapters/item.js
@@ -18,22 +18,36 @@ var chapters = {
         if (typeof startApp === 'function') {
 
             // Load Data
+            $.LoadingOverlay("show", { background: "rgba(0,0,0, 0.5)" });
             for (let i = 0; i < chapters.amount; i++) {
+
+                // Data
                 const chapter = i + 1;
-                $.getJSON('/chapters/' + chapters.lang + '/' + chapter + '.json',
-                    function(data) {
+                $.getJSON('./chapters/' + chapters.lang + '/' + chapter + '.json')
 
-                        // Insert Data
-                        chapters[chapter] = data;
+                // Complete
+                .done(function(data) {
 
-                        // Complete
-                        chapters.count++;
-                        if (chapters.count === chapters.amount) {
-                            startApp();
-                        }
+                    // Insert Data
+                    chapters[chapter] = data;
 
+                    // Complete
+                    chapters.count++;
+                    if (chapters.count === chapters.amount) {
+                        startApp(function() { $.LoadingOverlay("hide"); });
                     }
-                );
+
+                })
+
+                // Fail
+                .fail(function(err) {
+
+                    console.error(err);
+                    alert(err.message);
+                    $.LoadingOverlay("hide");
+
+                });
+
             }
 
         }
--- docs/js/files/start.js
+++ docs/js/files/start.js
@@ -1,8 +1,9 @@
 // Start Load
-$(function() {
+$(function(fn) {
     chapters.start(function() {
 
         console.log(chapters);
+        fn();
 
     });
 });
