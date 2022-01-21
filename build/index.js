c2367b41baa5f55138c87e6e9b1f14caab7f1633
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-21 13:36:44 -0300

    test app added to build

---
+++ build/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '../docs')));
+
+app.listen(port, () => {
+    console.log(`Test app listening on port ${port}`);
+}) l
