2b59b61ba802f9d69ebc2e221f3d54e2690f9d7a
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-23 12:21:14 -0300

    HTML generator added

--- build/characters/index.js
+++ build/characters/index.js
@@ -36,6 +36,64 @@ fs.readdir(folderPath, (err, files) => {
                 version: '1.0'
             }, null, 2));
 
+            // Create HTML File
+            fs.writeFileSync(path.join(ficData.path, './characters/' + file.name + '.html'), `
+<html lang="en">
+    <head>
+        
+        <!-- Config -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8 X-Content-Type-Options=nosniff">
+        <meta http-equiv="Content-Script-Type" content="text/javascript">
+        
+        <!-- Title -->
+        <title>${jsonFile.title}</title>
+        <link href="/img/icon/icon.png" rel="icon" type="image/x-icon"/>
+        
+        <!-- CSS -->
+        <link rel="preload" href="/css/bootstrap.min.css" as="style">
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/dark.css">
+        <link rel="stylesheet" href="/css/main.css">
+        <link rel="manifest" href="/manifest.json">
+
+        <!-- Page Info -->
+        <meta content="${jsonFile.title}" property="og:title">
+        <meta content="https://${ficData.config.domain}/characters/${file.name}.html" property="og:url">
+        <meta property="url" content="https://${ficData.config.domain}/characters/${file.name}.html">
+        <meta content="${jsonFile.author_name}" property="dc:creator">
+        <meta content="website" property="og:type">
+        <meta content="${jsonFile.description}" property="og:description">
+        <meta content="${jsonFile.description}" property="description">
+
+        <!-- Embed -->
+        <link href="/oEmbed/characters/${file.name}.json" rel="alternate" title="oEmbed JSON Profile" type="application/json+oembed">
+        <meta content="/oEmbed/characters/${file.name}.${jsonFile.image}" property="og:image">
+        <link href="https://${ficData.config.domain}/characters/${file.name}.html" rel="canonical">
+        
+        <!-- Theme -->
+        <meta name="theme-color" content="${jsonFile.color}">
+        <meta name="msapplication-navbutton-color" content="${jsonFile.color}">
+        <meta name="apple-mobile-web-app-status-bar-style" content="${jsonFile.color}">
+
+        <!-- Website -->
+        <meta property="og:type" content="website">
+        <meta property="og:site_name" content="${ficData.config.title}">
+        <meta property="og:site" content="${ficData.config.title}">
+        <meta property="twitter:site" content="${ficData.config.title}">
+
+        <!-- Script -->
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/loadingoverlay.min.js"></script>
+        <script src="/js/files/redirect.js"></script>
+    
+    </head>
+    <body><div id="newURL" href="/?path=%2Fimg%2Fcharacters%2F${file.name}%2FREADME.md&title=${encodeURIComponent(jsonFile.title)}"></div></body>
+
+</html>
+            `);
+
             // End Group
             console.groupEnd();
 
