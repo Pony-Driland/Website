8bbdf71a9196740982580a0bf1be7ed6a3c23b7c
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-26 00:17:43 -0300

    Chapter Cache

--- docs/js/chapter_manager/updater.js
+++ docs/js/chapter_manager/updater.js
@@ -2,18 +2,19 @@ $(window).scroll(function(event) {
     var scroll = $(window).scrollTop();
     // Do something
     // Detectar qual as divs visiveis no momento. Sempre focar na div que está visivel na página atualmente.
+    // updateChapterCache();
 });
 
 var updateChapterCache = function(lastPage) {
     storyData.chapter.line = lastPage;
-    for (let i = 0; i < lastPage; i++) {
+    const data = storyData.data[storyData.chapter.selected];
+    for (const i in data) {
 
         // Get Data
-        const data = storyData.data[storyData.chapter.selected][i];
         if (data.set) {
-            for (const item in data.set) {
+            for (const item in data[i].set) {
                 if (typeof chapterSet[item] === 'function') {
-                    chapterSet[item](data.set[item]);
+                    chapterSet[item](data[i].set[item], (i < lastPage));
                 }
             }
         }
@@ -24,16 +25,16 @@ var updateChapterCache = function(lastPage) {
 // Set Actions
 var chapterSet = {
 
-    day: function(value) {
-        console.log(value);
+    day: function(value, actionFromNow = false) {
+        console.log(value, actionFromNow);
     },
 
-    dayNightCycle: function(value) {
-        console.log(value);
+    dayNightCycle: function(value, actionFromNow = false) {
+        console.log(value, actionFromNow);
     },
 
-    where: function(value) {
-        console.log(value);
+    where: function(value, actionFromNow = false) {
+        console.log(value, actionFromNow);
     }
 
 };
