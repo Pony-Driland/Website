334817a643d6bd8aa4e2b9cd7c4ef3faf4c936ec
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-26 15:14:58 -0300

    table color

--- docs/js/chapter_manager/index.js
+++ docs/js/chapter_manager/index.js
@@ -198,7 +198,7 @@ var openChapterMenu = function(params = {}) {
             tinyPag,
 
             // Table
-            $('<table>', { class: 'table table-bordered' }).append(table),
+            $('<table>', { class: 'table table-bordered' }).css('background-color', 'rgb(44 44 44)').append(table),
 
             // Pagination
             tinyPag2,
