8fd0cb204807adfbf712c40f112ad21bbb76bae0
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-31 18:25:21 -0300

    onPlaying

--- docs/js/chapter_manager/updater.js
+++ docs/js/chapter_manager/updater.js
@@ -119,19 +119,35 @@ var musicManager = {
 
         if (storyData.music.nav) {
 
+            // View
             $('#music-player').addClass('border').removeClass('d-none').addClass('mr-3');
 
+            // Buff
             if (storyData.music.buffering) {
                 $('#music-player > a').addClass('disabled');
             } else {
                 $('#music-player > a').removeClass('disabled');
             }
 
+            // Playing
             if (storyData.music.playing) {
                 storyData.music.nav.play.addClass('fa-pause').removeClass('fa-play');
-            } else if (storyData.music.paused || storyData.music.stoppabled) {
+            } else if (storyData.music.paused) {
+                storyData.music.nav.play.addClass('fa-play').removeClass('fa-pause');
+            } else if (
+                storyData.music.stoppabled ||
+                typeof storyData.music.currentTime !== 'number' || typeof storyData.music.duration !== 'number' ||
+                storyData.music.currentTime === storyData.music.duration
+            ) {
                 storyData.music.nav.play.addClass('fa-play').removeClass('fa-pause');
             }
+
+            // Volume
+            if (typeof storyData.music.volume === 'number') {
+
+            }
+
+            // Time
 
         }
 
@@ -139,6 +155,15 @@ var musicManager = {
 
     // Start Base
     startBase: function() {
+
+        // Add Youtube Playing Detector
+        if (appData.youtube && !appData.youtube.onPlaying) {
+            appData.youtube.onPlaying = function() {
+                storyData.music.currentTime = storyData.youtube.currentTime;
+                storyData.music.duration = storyData.youtube.duration;
+                musicManager.updatePlayer();
+            };
+        }
 
         // Add Item Base
         if ($('#fic-nav > #status #music').length < 1) {
@@ -176,11 +201,6 @@ var musicManager = {
                     $('<a>', { href: 'javascript:void(0)' }).append(storyData.music.nav.stop)
                 )
             );
-
-            // Add Playing Detector
-            appData.youtube.onPlaying = function() {
-                //console.log(storyData.youtube.currentTime);
-            };
 
             // Insert
             $('#fic-nav > #status').prepend(storyData.chapter.nav.music);
