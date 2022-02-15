0a91a38550b5112a45a59c59cf716d4b855d5000
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-02-15 20:45:27 -0300

    temp

--- docs/js/chapter_manager/index.js
+++ docs/js/chapter_manager/index.js
@@ -3,22 +3,28 @@ const rainConfig = {};
 (function() {
 
     var increment = 0;
-    rainConfig.drops = "";
-    rainConfig.backDrops = "";
 
-    while (increment < 100) {
-        //couple random numbers to use for various randomizations
-        //random number between 98 and 1
-        var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
-        //random number between 5 and 2
-        var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
-        //increment
-        increment += randoFiver;
-        //add in a new raindrop with various randomizations to certain CSS properties
-        rainConfig.drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
-        rainConfig.backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
-    }
+    rainConfig.drops = $('<div>', { class: 'drop' }).append(
 
+    );
+
+    rainConfig.backDrops = $('<div>', { class: 'drop' }).append(
+
+    );
+    /* 
+        while (increment < 100) {
+            //couple random numbers to use for various randomizations
+            //random number between 98 and 1
+            var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+            //random number between 5 and 2
+            var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+            //increment
+            increment += randoFiver;
+            //add in a new raindrop with various randomizations to certain CSS properties
+            rainConfig.drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+            rainConfig.backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+        }
+     */
     return;
 
 })();
