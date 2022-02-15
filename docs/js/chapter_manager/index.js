8a4dfd8d0c8cbe49b03a1d4a7f45534ec4bf7501
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-02-15 20:26:05 -0300

    Rain added

--- docs/js/chapter_manager/index.js
+++ docs/js/chapter_manager/index.js
@@ -1,3 +1,34 @@
+/*  Rain made by Aaron Rickle */
+(function() {
+
+    var increment = 0;
+    var drops = "";
+    var backDrops = "";
+
+    while (increment < 100) {
+        //couple random numbers to use for various randomizations
+        //random number between 98 and 1
+        var randoHundo = (Math.floor(Math.random() * (98 - 1 + 1) + 1));
+        //random number between 5 and 2
+        var randoFiver = (Math.floor(Math.random() * (5 - 2 + 1) + 2));
+        //increment
+        increment += randoFiver;
+        //add in a new raindrop with various randomizations to certain CSS properties
+        drops += '<div class="drop" style="left: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+        backDrops += '<div class="drop" style="right: ' + increment + '%; bottom: ' + (randoFiver + randoFiver - 1 + 100) + '%; animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"><div class="stem" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div><div class="splat" style="animation-delay: 0.' + randoHundo + 's; animation-duration: 0.5' + randoHundo + 's;"></div></div>';
+    }
+
+    return;
+
+})();
+
+// Start Rain
+var startRain = function() {
+    $('.rain').empty();
+    $('.rain.front-row').append(drops);
+    $('.rain.back-row').append(backDrops);
+};
+
 var storyDialogue = {
 
     nsfwChecker: function(data) {
@@ -244,6 +275,8 @@ var openChapterMenu = function(params = {}) {
 
             // Night Effects
             $('<div>', { id: 'bg-sky' }).append(
+                $('<div>', { class: 'rain front-row' }),
+                $('<div>', { class: 'rain back-row' }),
                 $('<div>', { class: 'stars' }),
                 $('<div>', { class: 'twinkling' }),
                 $('<div>', { class: 'clouds' })
@@ -257,6 +290,7 @@ var openChapterMenu = function(params = {}) {
         // Complete
         $(window).trigger('scroll');
         if (line !== null) { tinyLib.goToByScroll($('#markdown-read [line="' + line + '"]'), 0); }
+        startRain();
         return;
 
     };
