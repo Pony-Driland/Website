fa39922e53334d4dfe25cc2d6bab752a0c0fee9e
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-02-16 15:43:58 -0300

    fix

--- docs/js/chapter_manager/music.js
+++ docs/js/chapter_manager/music.js
@@ -532,6 +532,10 @@ musicManager.insertSFX = function(item, loop = true) {
 
         if (!storyData.sfx[item]) {
 
+            // Prepare
+            storyData.sfx[item] = {};
+
+            // Try
             try {
 
                 // File URL
@@ -558,158 +562,155 @@ musicManager.insertSFX = function(item, loop = true) {
                         newSound.addUri(url, file.duration * 1000, item);
                         newSound.callback(function() {
 
-                            storyData.sfx[item] = {
+                            // File
+                            storyData.sfx[item].data = newSound;
+                            storyData.sfx[item].file = file;
 
-                                // File
-                                data: newSound,
-                                file: file,
+                            // Values
+                            storyData.sfx[item].playing = false;
+                            storyData.sfx[item].loop = true;
+                            storyData.sfx[item].hiding = false;
+                            storyData.sfx[item].volume = newSound._volume * 100;
 
-                                // Values
-                                playing: false,
-                                loop: true,
-                                hiding: false,
-                                volume: newSound._volume * 100,
+                            // Stop
+                            storyData.sfx[item].stop = function() {
+                                if (storyData.sfx[item].playing) {
+                                    storyData.sfx[item].playing = false;
+                                    newSound.stop();
+                                }
+                            };
 
-                                // Stop
-                                stop: function() {
+                            storyData.sfx[item].start = function() {
+                                if (!storyData.sfx[item].playing) {
+                                    storyData.sfx[item].playing = true;
+                                    newSound.start(item);
+                                }
+                            };
+
+                            // Play
+                            storyData.sfx[item].play = function(volume = null) {
+                                if (storyData.sfx[item].hiding) { storyData.sfx[item].stop(); }
+                                storyData.sfx[item].hiding = false;
+                                storyData.sfx[item].showing = false;
+                                return new Promise(function(resolve, reject) {
+                                    setTimeout(function() {
+                                        try {
+
+                                            if (typeof volume === 'number') {
+                                                storyData.sfx[item].setVolume(volume);
+                                            } else { storyData.sfx[item].setVolume(storyData.sfx[item].volume); }
+
+                                            storyData.sfx[item].start();
+                                            resolve();
+
+                                        } catch (err) { reject(err); }
+                                    }, 1);
+                                });
+                            };
+
+                            // Set Volume
+                            storyData.sfx[item].setVolume = function(value, notEdit = false) {
+                                return new Promise(function(resolve) {
+
+                                    let tinyValue = value;
+                                    if (typeof tinyValue !== 'number') {
+                                        tinyValue = storyData.sfx[item].volume;
+                                    }
+
+                                    if (tinyValue > 100) {
+                                        tinyValue = 100;
+                                    } else if (tinyValue < 0) {
+                                        tinyValue = 0;
+                                    }
+
+                                    let newVolume = tinyLib.rule3(tinyValue, 100, storyData.music.volume);
+                                    if (newVolume > 100) { newVolume = 100; }
+                                    if (newVolume < 0) { newVolume = 0; }
+
+                                    if (notEdit && newVolume > tinyValue) {
+                                        newVolume = tinyValue;
+                                    }
+
                                     if (storyData.sfx[item].playing) {
-                                        storyData.sfx[item].playing = false;
-                                        newSound.stop();
-                                    }
-                                },
-
-                                start: function() {
-                                    if (!storyData.sfx[item].playing) {
-                                        storyData.sfx[item].playing = true;
-                                        newSound.start(item);
-                                    }
-                                },
-
-                                // Play
-                                play: function(volume = null) {
-                                    if (storyData.sfx[item].hiding) { storyData.sfx[item].stop(); }
-                                    storyData.sfx[item].hiding = false;
-                                    storyData.sfx[item].showing = false;
-                                    return new Promise(function(resolve, reject) {
-                                        setTimeout(function() {
-                                            try {
-
-                                                if (typeof volume === 'number') {
-                                                    storyData.sfx[item].setVolume(volume);
-                                                } else { storyData.sfx[item].setVolume(storyData.sfx[item].volume); }
-
-                                                storyData.sfx[item].start();
-                                                resolve();
-
-                                            } catch (err) { reject(err); }
-                                        }, 1);
-                                    });
-                                },
-
-                                // Set Volume
-                                setVolume: function(value, notEdit = false) {
-                                    return new Promise(function(resolve) {
-
-                                        let tinyValue = value;
-                                        if (typeof tinyValue !== 'number') {
-                                            tinyValue = storyData.sfx[item].volume;
-                                        }
-
-                                        if (tinyValue > 100) {
-                                            tinyValue = 100;
-                                        } else if (tinyValue < 0) {
-                                            tinyValue = 0;
-                                        }
-
-                                        let newVolume = tinyLib.rule3(tinyValue, 100, storyData.music.volume);
-                                        if (newVolume > 100) { newVolume = 100; }
-                                        if (newVolume < 0) { newVolume = 0; }
-
-                                        if (notEdit && newVolume > tinyValue) {
-                                            newVolume = tinyValue;
-                                        }
-
-                                        if (storyData.sfx[item].playing) {
-                                            newSound.volume(newVolume / 100);
-                                        }
-
-                                        if (!notEdit) {
-                                            storyData.sfx[item].volume = tinyValue;
-                                        }
-
-                                        resolve();
-
-                                    });
-                                },
-
-                                // Hide
-                                hide: async function(hideTimeout = 50) {
-
-                                    let volume = newSound._volume * 100;
-
-                                    storyData.sfx[item].hiding = true;
-                                    storyData.sfx[item].showing = false;
-
-                                    for (let i = 0; i < 100; i++) {
-                                        if (storyData.sfx[item].hiding) {
-                                            await new Promise(function(resolve) {
-                                                setTimeout(function() {
-                                                    volume--;
-                                                    storyData.sfx[item].setVolume(volume, true);
-                                                    resolve();
-                                                }, hideTimeout);
-                                            });
-                                        }
+                                        newSound.volume(newVolume / 100);
                                     }
 
+                                    if (!notEdit) {
+                                        storyData.sfx[item].volume = tinyValue;
+                                    }
+
+                                    resolve();
+
+                                });
+                            };
+
+                            // Hide
+                            storyData.sfx[item].hide = async function(hideTimeout = 50) {
+
+                                let volume = newSound._volume * 100;
+
+                                storyData.sfx[item].hiding = true;
+                                storyData.sfx[item].showing = false;
+
+                                for (let i = 0; i < 100; i++) {
                                     if (storyData.sfx[item].hiding) {
-                                        storyData.sfx[item].stop();
-                                        storyData.sfx[item].hiding = false;
-                                        storyData.sfx[item].showing = false;
+                                        await new Promise(function(resolve) {
+                                            setTimeout(function() {
+                                                volume--;
+                                                storyData.sfx[item].setVolume(volume, true);
+                                                resolve();
+                                            }, hideTimeout);
+                                        });
                                     }
+                                }
 
-                                },
-
-
-                                // Show
-                                show: async function(hideTimeout = 50) {
-
+                                if (storyData.sfx[item].hiding) {
                                     storyData.sfx[item].stop();
-
                                     storyData.sfx[item].hiding = false;
                                     storyData.sfx[item].showing = false;
-
-                                    const soundVolume = storyData.sfx[item].volume;
-                                    let volume = 0;
-                                    storyData.sfx[item].showing = true;
-                                    storyData.sfx[item].hiding = false;
-                                    storyData.sfx[item].setVolume(0, true);
-                                    storyData.sfx[item].start();
-
-                                    for (let i = 0; i < 100; i++) {
-                                        if (storyData.sfx[item].showing) {
-                                            await new Promise(function(resolve) {
-                                                setTimeout(function() {
-                                                    if (volume < soundVolume) {
-                                                        volume++;
-                                                        storyData.sfx[item].setVolume(volume, true);
-                                                    } else {
-                                                        storyData.sfx[item].setVolume(soundVolume, true);
-                                                    }
-                                                    resolve();
-                                                }, hideTimeout);
-                                            });
-                                        }
-                                    }
-
-                                    if (storyData.sfx[item].showing) {
-                                        storyData.sfx[item].hiding = false;
-                                        storyData.sfx[item].showing = false;
-                                    }
-
                                 }
 
                             };
+
+
+                            // Show
+                            storyData.sfx[item].show = async function(hideTimeout = 50) {
+
+                                storyData.sfx[item].stop();
+
+                                storyData.sfx[item].hiding = false;
+                                storyData.sfx[item].showing = false;
+
+                                const soundVolume = storyData.sfx[item].volume;
+                                let volume = 0;
+                                storyData.sfx[item].showing = true;
+                                storyData.sfx[item].hiding = false;
+                                storyData.sfx[item].setVolume(0, true);
+                                storyData.sfx[item].start();
+
+                                for (let i = 0; i < 100; i++) {
+                                    if (storyData.sfx[item].showing) {
+                                        await new Promise(function(resolve) {
+                                            setTimeout(function() {
+                                                if (volume < soundVolume) {
+                                                    volume++;
+                                                    storyData.sfx[item].setVolume(volume, true);
+                                                } else {
+                                                    storyData.sfx[item].setVolume(soundVolume, true);
+                                                }
+                                                resolve();
+                                            }, hideTimeout);
+                                        });
+                                    }
+                                }
+
+                                if (storyData.sfx[item].showing) {
+                                    storyData.sfx[item].hiding = false;
+                                    storyData.sfx[item].showing = false;
+                                }
+
+                            };
+
 
                             storyData.sfx[item].pizzicato = new Pizzicato.Sound({
                                 source: 'file',
@@ -729,7 +730,13 @@ musicManager.insertSFX = function(item, loop = true) {
 
                 } else { reject(new Error('Invalid SFX File! ' + item)); }
 
-            } catch (err) { reject(err); }
+            }
+
+            // Fail
+            catch (err) {
+                delete storyData.sfx[item];
+                reject(err);
+            }
 
         }
 
