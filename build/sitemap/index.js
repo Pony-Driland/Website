075535eb77f1fe48d17d17aa1e415a7347698df0
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-02-20 12:29:30 -0300

    xml update

--- build/sitemap/index.js
+++ build/sitemap/index.js
@@ -6,6 +6,7 @@ const { SitemapStream } = require('sitemap');
 // Get Fic Data
 const ficData = require('../publicFolder')();
 const chapterMap = fs.createWriteStream(path.join(ficData.path, './sitemap/chapter.xml'));
+const charactersMap = fs.createWriteStream(path.join(ficData.path, './sitemap/characters.xml'));
 console.log(ficData);
 
 // Read Data
@@ -28,6 +29,31 @@ fs.readdir(folderPath, (err, files) => {
         // Complete
         smChapter.pipe(chapterMap);
         smChapter.end();
+
+        const folderPath = path.join(ficData.path, './characters');
+        fs.readdir(folderPath, (err, files) => {
+            if (!err) {
+
+                // Data
+                const data = { count: 0 };
+                const smCharacters = new SitemapStream({ hostname: 'https://' + ficData.config.domain + '/' });
+
+                // Read Files
+                files.forEach(file => {
+
+                    data.count++;
+                    smCharacters.write({ url: '/characters/' + file, changefreq: 'daily', priority: 0.3 });
+
+                });
+
+                // Complete
+                smCharacters.pipe(charactersMap);
+                smCharacters.end();
+
+            } else {
+                console.error(err);
+            }
+        });
 
     } else {
         console.error(err);
