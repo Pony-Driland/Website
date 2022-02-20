9337d2d1b5e55f003a41918a59f2590684fb6d7f
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-02-20 11:40:34 -0300

    preparing sitemap

---
+++ build/sitemap/index.js
@@ -0,0 +1,39 @@
+// Get Path
+const path = require('path');
+const fs = require('fs');
+const { SitemapStream } = require('sitemap');
+
+// Get Fic Data
+const fileName = 'sitemap.xml';
+const ficData = require('../publicFolder')();
+const smStream = new SitemapStream({ hostname: 'https://' + ficData.domain + '/' });
+console.log(ficData);
+
+// Read Data
+const folderPath = path.join(ficData.path, './chapters/' + ficData.config.defaultLang);
+fs.readdir(folderPath, (err, files) => {
+    if (!err) {
+
+        // Data
+        const data = { count: 0 };
+
+        // Read Files
+        files.forEach(file => {
+
+            data.count++;
+            smStream.write({ url: '/chapter.html?v=' + data.count, changefreq: 'daily', priority: 0.3 });
+
+        });
+
+        smStream.end();
+        console.log(smStream);
+
+        console.log(data);
+
+    } else {
+        console.error(err);
+    }
+});
+
+// Interval
+setInterval(function() {}, 100);
