c17bc562a9a8468112684a727d55ca81634705fc
Yasmin Seidel <tiny@puddy.club>
2022-08-31 14:06:45 -0300

    antiCors issue

--- build/index.js
+++ build/index.js
@@ -6,6 +6,9 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../docs')));
 
+const antiCors = require('./antiCors');
+antiCors(app);
+
 app.listen(port, () => {
     console.log(`Test app listening on port ${port}`);
 });
