59dac3291309845951a4e0464ba20b95b4918518
Yasmin Seidel <tiny@puddy.club>
2022-08-31 22:51:10 -0300

    song file migration

---
+++ docs/chapters/sound.js
@@ -0,0 +1,278 @@
+// Playlist
+storyCfg.playlist= {
+
+    'madoka-magica-magia【Metal/Male Cover】': [
+        { id: 'FhhbYF8JANg', type: 'youtube' },
+    ],
+
+    'and-in-darkness-may-she-reign': [
+        { id: '3KONnESTUxs', type: 'youtube' },
+    ],
+
+    'an-antagonistic-battle': [
+        { id: 'AhpvfGJ81n4', type: 'youtube' },
+    ],
+
+    'dbs-ost-dramatic-versions-extended-mix': [
+        { id: 'T-uG8gx8nfw', type: 'youtube' },
+    ],
+
+    'dramatic': [
+        { id: 'i1IUSdcauC8', type: 'youtube' },
+        { id: '1XmMTRD_zys', type: 'youtube' },
+    ],
+
+    'dramatic-epic': [
+        { id: 'iVtDQhGRo1Q', type: 'youtube' },
+        { id: 'gIjSCwPVcU4', type: 'youtube' },
+    ],
+
+    'suspenseful-ambient': [
+        { id: '6_Na6QaG0c4', type: 'youtube' },
+        { id: 'fFJHAbeQ0g4', type: 'youtube' },
+    ],
+
+    'haunted-coal-mines': [
+        { id: 'VCpg4CbgY1U', type: 'youtube' },
+    ],
+
+    'cryptozoologist': [
+        { id: 'cKY5PVuopWs', type: 'youtube' },
+    ],
+
+    'tobbogan-ride': [
+        { id: 'pRE2EjzmeI0', type: 'youtube' },
+    ],
+
+    'forest': [
+        { id: '3yxG4k9uni4', type: 'youtube' },
+        { id: 'pI_JSnJMnY4', type: 'youtube' },
+        { id: 'gvbvkqpM9C8', type: 'youtube' },
+    ],
+
+    'dark-ambient-melodies': [
+        { id: '6r3xpB6Td40', type: 'youtube' },
+        { id: 'lAarDAnoXk0', type: 'youtube' },
+        { id: 'LptTKfrHSi4', type: 'youtube' },
+    ],
+
+    'encouraging-emotional': [
+        { id: 'CfCkb8VWNPI', type: 'youtube' },
+    ],
+
+    'silly': [
+        { id: 'ljTfHitG0eA', type: 'youtube' },
+        { id: '5Im8S1l3UK0', type: 'youtube' }
+    ],
+
+    'relaxing-rpg-song': [
+        { id: 'l6veMGLpol0', type: 'youtube' },
+    ],
+
+    'nuclear-sound': [
+        { id: '1ulfXmwvKhI', type: 'youtube', once: true },
+    ],
+
+    'escape': [
+        { id: '3jw04mIF3fs', type: 'youtube' },
+        { id: '_j8jUUzGqDo', type: 'youtube' },
+        { id: 'FaKRXOgasXA', type: 'youtube' },
+    ],
+
+    'battle': [
+        { id: 'VMziMXYkVS0', type: 'youtube' },
+        { id: 'rjRn9lYguV0', type: 'youtube' },
+        { id: 'yb0-hwJAX3U', type: 'youtube' },
+        { id: 'XD72-Y5aB9Q', type: 'youtube' },
+    ],
+
+    'tense-battle': [
+        { id: 'b6WhRKOMUq0', type: 'youtube' },
+        { id: 'rAMJkRhK2cs', type: 'youtube' },
+    ],
+
+    'horror-battle': [
+        { id: 'ViCGaoeCUqk', type: 'youtube' },
+    ],
+
+    'revenge-battle': [
+        { id: 'Ies0Ynk5_SI', type: 'youtube' },
+    ],
+
+    'epic-battle': [
+        { id: '4tM--a1NLa8', type: 'youtube' },
+        { id: 'ltmma6Abi3U', type: 'youtube' },
+        { id: 'f3Fi4q7Es2Q', type: 'youtube' },
+        { id: 'HRfiZbJ5kW0', type: 'youtube' },
+        { id: '2waixMvzakE', type: 'youtube' },
+        { id: 'ACXW__UAC7w', type: 'youtube' },
+    ],
+
+    'drama-battle': [
+        { id: '04_v0iVxCHo', type: 'youtube' },
+        { id: '9HvVidW5N6w', type: 'youtube' },
+        { id: 'GRoIRQEI1W8', type: 'youtube' },
+        { id: '2SsRDysiIlk', type: 'youtube' },
+    ],
+
+    'happyness': [
+        { id: 'AVxQSpnR7g8', type: 'youtube' },
+        { id: 'uFKo2LYu2Zg', type: 'youtube' },
+    ],
+
+    'tech-problem': [
+        { id: 'bGdD0YVh_kI', type: 'youtube' },
+    ],
+
+    'fantasy-village': [
+        { id: 'kKObUC_3NDY', type: 'youtube' },
+        { id: 'HFoGnJ_w4pQ', type: 'youtube' },
+        { id: 'K1vUA9NltVw', type: 'youtube' },
+    ],
+
+    'calm': [
+        { id: '6wmRYL5ZdlM', type: 'youtube' },
+        { id: 'mxcu9L9j8ww', type: 'youtube' },
+        { id: 'dZR7M7gDNSc', type: 'youtube' },
+        { id: '7Uyu5BrGMs4', type: 'youtube' },
+        { id: 'TOEiLlspdhM', type: 'youtube' },
+        { id: '3VoGUhPwagI', type: 'youtube' },
+        { id: 'uU4VgCWuAxE', type: 'youtube' },
+        { id: 'dx0YwqEue_M', type: 'youtube' },
+        { id: 'XYoTrKYkk1w', type: 'youtube' },
+        { id: 'nl4zbChDJsM', type: 'youtube' },
+    ],
+
+    'memories': [
+        { id: 'Cste0PD-Xt4', type: 'youtube' },
+        { id: 'A7DE3BXdPsA', type: 'youtube' },
+    ],
+
+    'kanashi': [
+        { id: 'zaxVBfdgR3E', type: 'youtube' },
+    ],
+
+    'dark-and-mysterious-ambient': [
+        { id: 'yEYxnJB4jZs', type: 'youtube' },
+    ],
+
+    'spooky-mysterious': [
+        { id: 'm6e_Uz6vlJw', type: 'youtube' },
+        { id: 'XUUM0cKcRW0', type: 'youtube' },
+        { id: 'eaEbST63kk0', type: 'youtube' },
+    ],
+
+    'tense-mystery': [
+        { id: 'YLrJPBbAQoY', type: 'youtube' },
+        { id: 'gzQzViNj5go', type: 'youtube' },
+        { id: 'Nziqug75Gn4', type: 'youtube' },
+        { id: '7dKxHERhaLE', type: 'youtube' },
+        { id: 'QNwjH5EKTTQ', type: 'youtube' },
+        { id: '1nBatlpsVj0', type: 'youtube' },
+        { id: 'g7x-VUGjnc8', type: 'youtube' },
+        { id: 'bE__iQGLxyc', type: 'youtube' },
+    ],
+
+    'tense-mystery-2': [
+        { id: 'JuKONvqIT4M', type: 'youtube' }
+    ],
+
+    'judgement-battle-divine': [
+        { id: '1xkvsu72i60', type: 'youtube' }
+    ],
+
+    'rainbow-queen-moment': [
+        { id: 's2bGUY97o6E', type: 'youtube' },
+    ],
+
+    'bad-mystery': [
+        { id: 'eeoEQZH-iwg', type: 'youtube' },
+        { id: '_x8gYuNUdNA', type: 'youtube' },
+        { id: 'DJlgRlhaeME', type: 'youtube' },
+        { id: 'VQ9230-OoBw', type: 'youtube' },
+        { id: 'vlQvh7IrRPk', type: 'youtube' },
+    ],
+
+    'the-true': [
+        { id: 'XheH4qClv1Q', type: 'youtube' },
+        { id: '_x8gYuNUdNA', type: 'youtube' },
+    ],
+
+    'sad': [
+        { id: 'cU96ZIGKars', type: 'youtube' },
+        { id: 'lIjHZy3aYDA', type: 'youtube' },
+        { id: 'eqV2_PDDVr4', type: 'youtube' },
+        { id: '5i0N9F--gPU', type: 'youtube' },
+        { id: 'bL5xpXvmGiM', type: 'youtube' },
+        { id: '2lj9kr1LmQ4', type: 'youtube' },
+        { id: 'JKgPqJZm1-I', type: 'youtube' },
+        { id: 'FSkYYV62FEk', type: 'youtube' },
+        { id: '4VFrTREyIig', type: 'youtube' },
+        { id: 'E5I667E4GXw', type: 'youtube' },
+        { id: '24QJV6NnFHY', type: 'youtube' },
+        { id: 'YhXWzeFJtqM', type: 'youtube' },
+        { id: 'ewBkq44wynk', type: 'youtube' },
+    ],
+
+    'think': [
+        { id: 'Y36Rg14JcZE', type: 'youtube' },
+        { id: '6oaXQ_JQY64', type: 'youtube' },
+        { id: 'ZXNxF7kVW_8', type: 'youtube' },
+        { id: 'UDhQ8DarZHQ', type: 'youtube' },
+        { id: 'yNACbXekBVQ', type: 'youtube' },
+        { id: 'yKwdd2Aja_k', type: 'youtube' },
+        { id: 'DVrsjXSpH9o', type: 'youtube' },
+        { id: 'BAB7dhJpqes', type: 'youtube' },
+        { id: '4c7Juty0RYI', type: 'youtube' },
+        { id: 'ncCnW7MKTCU', type: 'youtube' },
+        { id: 'TMLnDTkUEgg', type: 'youtube' },
+        { id: '6yPivtTKLno', type: 'youtube' },
+        { id: 'SzhjAqPF-gM', type: 'youtube' },
+        { id: 'k79T3jVvPjw', type: 'youtube' },
+        { id: 'oxivFag3Reg', type: 'youtube' },
+        { id: 'ni-Q2RCoexg', type: 'youtube' },
+    ],
+
+    'good-think': [
+        { id: '5Im8S1l3UK0', type: 'youtube' },
+    ],
+
+    'nuclear-winter': [
+        { id: 'cRzQuPvLjdU', type: 'youtube' },
+    ],
+
+    'bad-think': [
+        { id: '0ZD3SfS9x8E', type: 'youtube' },
+        { id: 'AfkEAnazGMo', type: 'youtube' },
+        { id: 'QD5OGfAJIus', type: 'youtube' },
+        { id: 'eeeQcvtjV2k', type: 'youtube' },
+        { id: 'CAMmIyAA6CY', type: 'youtube' },
+        { id: 'Li2v4Afy1H0', type: 'youtube' },
+    ],
+
+    'sad-plot-twist': [
+        { id: '44A0UTwoCsg', type: 'youtube' },
+    ],
+
+    'new-thing': [
+        { id: 'PkIc6TMLfv0', type: 'youtube' },
+        { id: 'uV2eI9KBOFs', type: 'youtube' },
+        { id: 'E0wknC_56vE', type: 'youtube' },
+    ],
+
+    'troubles': [
+        { id: 'gWNsV8L1DB8', type: 'youtube' },
+    ],
+
+    'first-vs-rayane': [
+        { id: 'WspwWDFr4IM', type: 'youtube' },
+    ],
+
+};
+
+// SFX
+storyCfg.sfx = {
+    'heavy-rain-little-thunder': { type: 'file', module: 'main', value: '/audio/jasmindreasond/rain/heavy-rain-little-thunder.mp4' },
+    'heavy-rain': { type: 'file', module: 'main', value: '/audio/jasmindreasond/rain/heavy-rain.mp4' },
+    'ambulance-alarm': { type: 'file', module: 'main', value: '/audio/jasmindreasond/alarm/ambulance.wav' }
+};
