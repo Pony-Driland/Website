778f4f8523925fec3c08402ae0215a92709f2e19
Yasmin Seidel <tiny@puddy.club>
2022-09-02 00:39:27 -0300

    getBookMark

--- build/solidity/reader.sol
+++ build/solidity/reader.sol
@@ -5,7 +5,7 @@ contract PonyDrilandReader1 {
 
     address payable public owner;
     mapping (address => mapping (uint256 => uint256)) public bookmark;
-    mapping (address => mapping (string => uint256)) public filters;
+    mapping (address => mapping (string => uint256)) public nsfw_filter;
 
     // Constructor
     constructor() {
@@ -17,8 +17,8 @@ contract PonyDrilandReader1 {
         return owner;
     }
 
-    function getBookMark() public view returns (address) {
-        return owner;
+    function getBookMark(address account, uint256 chapter) external view returns (uint256) {
+        return bookmark[account][chapter];
     }
     
 }
