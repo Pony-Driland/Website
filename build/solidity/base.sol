1533d42e6c9ca82645245bf09f1286be8c33ee4e
Yasmin Seidel <tiny@puddy.club>
2022-09-02 19:43:02 -0300

    fix

--- build/solidity/base.sol
+++ build/solidity/base.sol
@@ -106,8 +106,6 @@ contract PonyDrilandBase {
     function transfer(address _to, uint256 _value) public returns (bool success) {
 
         // Validator
-        require(_to != address(msg.sender), "Hey! This is youself");
-        require(_value >= 0, "Invalid amount!");
         require(_value <= balances[address(msg.sender)], "Invalid amount!");
         require(balances[address(msg.sender)] >= 1, "You must have at least 1 token.");
 
