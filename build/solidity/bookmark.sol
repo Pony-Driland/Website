4135e673c02688cf05133c6633ba86b17f7cef9a
Yasmin Seidel <tiny@puddy.club>
2022-09-01 22:29:36 -0300

    sol files

--- build/solidity/bookmark.sol
+++ build/solidity/bookmark.sol
@@ -1,6 +1,10 @@
-pragma solidity 0.8.15;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.16;
 
 contract PonyDrilandBookmark {
+
+    address payable public owner;
+    mapping (address => mapping (uint => uint)) public bookmark;
 
     // Constructor
     constructor() {
--- build/solidity/nsfw.sol
+++ build/solidity/nsfw.sol
@@ -1,6 +1,10 @@
-pragma solidity 0.8.15;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.16;
 
 contract PonyDrilandNSFWFilter {
+
+    address payable public owner;
+    mapping (address => mapping (string => uint)) public filters;
 
     // Constructor
     constructor() {
