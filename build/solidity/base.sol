26395b18c23ecd25ccd39720abd6f71ef3a62627
Yasmin Seidel <tiny@puddy.club>
2022-09-03 23:56:47 -0300

    _amount

--- build/solidity/base.sol
+++ build/solidity/base.sol
@@ -42,17 +42,17 @@ contract PonyDrilandBase {
     }
 
     // Donation
-    function donate() payable public returns (bool success) {
+    function donate(uint256 _amount) payable public returns (bool success) {
         
         require(address(msg.sender) != address(owner), "You are not allowed to do this.");
-        require(msg.value >= 0, "Invalid amount!");
-        require(msg.value <= msg.sender.balance, "Invalid amount!");
+        require(_amount >= 0, "Invalid amount!");
+        require(_amount <= msg.sender.balance, "Invalid amount!");
         
-        (bool _sent, bytes memory _data) = owner.call{value: msg.value}("");
+        (bool _sent, bytes memory _data) = owner.call{value: _amount}("");
         require(_sent, "Failed to send Ether");
 
-        donations[msg.sender] = donations[msg.sender] + msg.value;
-        emit Donation(msg.sender, msg.value, _data);
+        donations[msg.sender] = donations[msg.sender] + _amount;
+        emit Donation(msg.sender, _amount, _data);
         return true;
         
     }
