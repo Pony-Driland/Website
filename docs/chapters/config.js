9c27e456c508df818b112177b85fa684e1e196ed
Yasmin Seidel <tiny@puddy.club>
2022-09-04 23:57:03 -0300

    prepare polygon cloud

--- docs/chapters/config.js
+++ docs/chapters/config.js
@@ -10,6 +10,8 @@ var storyCfg = {
     kofi: 'JasminDreasond',
 
     web3: {
+        blockchain: 'polygon',
+        contract: '0x5816b6e25d5224c0afe33b22f28b611a98ca0985',
         welcome: `Hello crypto pony, welcome to the Pony Driland Dapp! The dimension of lost creatures and the home of hope.`
     },
 
