92fd995c2e409c0ec8e08752ec80a8586b7ae62a
Yasmin Seidel <tiny@puddy.club>
2022-09-05 00:58:38 -0300

    config abi

--- docs/crypto/config.js
+++ docs/crypto/config.js
@@ -11,167 +11,241 @@ storyCfg.web3.abi.base = [
         "inputs": [],
         "stateMutability": "nonpayable", "type": "constructor"
     },
-    
+
     {
-        "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" },
-        { "indexed": false, "internalType": "uint256", "name": "chapter", "type": "uint256" },
-        { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }],
+        "anonymous": false, "inputs": [
+            { "indexed": true, "internalType": "address", "name": "from", "type": "address" },
+            { "indexed": false, "internalType": "uint256", "name": "chapter", "type": "uint256" },
+            { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }
+        ],
         "name": "Bookmark", "type": "event"
     },
 
     {
-        "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "value", "type": "address" }],
+        "anonymous": false, "inputs": [
+            { "indexed": true, "internalType": "address", "name": "value", "type": "address" }
+        ],
         "name": "Enable", "type": "event"
     },
 
     {
-        "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" },
-        { "indexed": false, "internalType": "string", "name": "value", "type": "string" }],
+        "anonymous": false, "inputs": [
+            { "indexed": true, "internalType": "address", "name": "from", "type": "address" },
+            { "indexed": false, "internalType": "string", "name": "value", "type": "string" }
+        ],
         "name": "Interaction", "type": "event"
     },
 
     {
-        "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" },
-        { "indexed": false, "internalType": "string", "name": "filter", "type": "string" },
-        { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }],
+        "anonymous": false, "inputs": [
+            { "indexed": true, "internalType": "address", "name": "from", "type": "address" },
+            { "indexed": false, "internalType": "string", "name": "filter", "type": "string" },
+            { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }
+        ],
         "name": "NsfwFilter", "type": "event"
     },
 
     {
-        "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" },
-        { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }],
+        "anonymous": false, "inputs": [
+            { "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" },
+            { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }
+        ],
         "name": "OwnershipTransferred", "type": "event"
     },
 
     {
-        "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" },
-        { "indexed": false, "internalType": "string", "name": "value", "type": "string" }],
+        "anonymous": false, "inputs": [
+            { "indexed": true, "internalType": "address", "name": "from", "type": "address" },
+            { "indexed": false, "internalType": "string", "name": "value", "type": "string" }
+        ],
         "name": "Volume", "type": "event"
     },
 
     {
-        "inputs": [{ "internalType": "address", "name": "", "type": "address" },
-        { "internalType": "uint256", "name": "", "type": "uint256" }],
-        "name": "bookmark", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "inputs": [
+            { "internalType": "address", "name": "", "type": "address" },
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
+        "name": "bookmark", "outputs": [
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
         "stateMutability": "view", "type": "function"
     },
 
     {
-        "inputs": [{ "internalType": "string", "name": "_name", "type": "string" },
-        { "internalType": "uint256", "name": "amount", "type": "uint256" }],
-        "name": "changeNsfwFilter", "outputs": [{ "internalType": "bool", "name": "success", "type": "bool" }],
+        "inputs": [
+            { "internalType": "string", "name": "_name", "type": "string" },
+            { "internalType": "uint256", "name": "amount", "type": "uint256" }
+        ],
+        "name": "changeNsfwFilter", "outputs": [
+            { "internalType": "bool", "name": "success", "type": "bool" }
+        ],
         "stateMutability": "nonpayable", "type": "function"
     },
 
     {
         "inputs": [],
-        "name": "enable", "outputs": [{ "internalType": "bool", "name": "success", "type": "bool" }],
+        "name": "enable", "outputs": [
+            { "internalType": "bool", "name": "success", "type": "bool" }
+        ],
         "stateMutability": "nonpayable", "type": "function"
     },
 
     {
-        "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
-        "name": "enabled", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "inputs": [
+            { "internalType": "address", "name": "", "type": "address" }
+        ],
+        "name": "enabled", "outputs": [
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
         "stateMutability": "view", "type": "function"
     },
 
     {
-        "inputs": [{ "internalType": "address", "name": "_account", "type": "address" },
-        { "internalType": "uint256", "name": "_chapter", "type": "uint256" }],
-        "name": "getBookmark", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-        "stateMutability": "view", "type": "function"
-    },
-
-    {
-        "inputs": [],
-        "name": "getInteractions", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
-        "stateMutability": "view", "type": "function"
-    },
-
-    {
-        "inputs": [{ "internalType": "address", "name": "_account", "type": "address" },
-        { "internalType": "string", "name": "_name", "type": "string" }],
-        "name": "getNsfwFilter", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "inputs": [
+            { "internalType": "address", "name": "_account", "type": "address" },
+            { "internalType": "uint256", "name": "_chapter", "type": "uint256" }
+        ],
+        "name": "getBookmark", "outputs": [
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
         "stateMutability": "view", "type": "function"
     },
 
     {
         "inputs": [],
-        "name": "getOwner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }],
+        "name": "getInteractions", "outputs": [
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
         "stateMutability": "view", "type": "function"
     },
 
     {
-        "inputs": [{ "internalType": "address", "name": "_account", "type": "address" }],
-        "name": "getVolume", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "inputs": [
+            { "internalType": "address", "name": "_account", "type": "address" },
+            { "internalType": "string", "name": "_name", "type": "string" }
+        ],
+        "name": "getNsfwFilter", "outputs": [
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
         "stateMutability": "view", "type": "function"
     },
 
     {
         "inputs": [],
-        "name": "getWallets", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "name": "getOwner", "outputs": [
+            { "internalType": "address", "name": "", "type": "address" }
+        ],
         "stateMutability": "view", "type": "function"
     },
 
     {
-        "inputs": [{ "internalType": "uint256", "name": "_chapter", "type": "uint256" },
-        { "internalType": "uint256", "name": "amount", "type": "uint256" }],
-        "name": "insertBookmark", "outputs": [{ "internalType": "bool", "name": "success", "type": "bool" }],
+        "inputs": [
+            { "internalType": "address", "name": "_account", "type": "address" }
+        ],
+        "name": "getVolume", "outputs": [
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
+        "stateMutability": "view", "type": "function"
+    },
+
+    {
+        "inputs": [],
+        "name": "getWallets", "outputs": [
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
+        "stateMutability": "view", "type": "function"
+    },
+
+    {
+        "inputs": [
+            { "internalType": "uint256", "name": "_chapter", "type": "uint256" },
+            { "internalType": "uint256", "name": "amount", "type": "uint256" }
+        ],
+        "name": "insertBookmark", "outputs": [
+            { "internalType": "bool", "name": "success", "type": "bool" }
+        ],
         "stateMutability": "nonpayable", "type": "function"
     },
 
     {
-        "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
-        "name": "interactions", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "inputs": [
+            { "internalType": "address", "name": "", "type": "address" }
+        ],
+        "name": "interactions", "outputs": [
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
         "stateMutability": "view", "type": "function"
     },
 
     {
         "inputs": [],
-        "name": "name", "outputs": [{ "internalType": "string", "name": "", "type": "string" }],
+        "name": "name", "outputs": [
+            { "internalType": "string", "name": "", "type": "string" }
+        ],
         "stateMutability": "view", "type": "function"
     },
 
     {
-        "inputs": [{ "internalType": "address", "name": "", "type": "address" },
-        { "internalType": "string", "name": "", "type": "string" }],
-        "name": "nsfw_filter", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "inputs": [
+            { "internalType": "address", "name": "", "type": "address" },
+            { "internalType": "string", "name": "", "type": "string" }
+        ],
+        "name": "nsfw_filter", "outputs": [
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
         "stateMutability": "view", "type": "function"
     },
 
     {
         "inputs": [],
-        "name": "owner", "outputs": [{ "internalType": "address payable", "name": "", "type": "address" }],
+        "name": "owner", "outputs": [
+            { "internalType": "address payable", "name": "", "type": "address" }
+        ],
         "stateMutability": "view", "type": "function"
     },
 
     {
-        "inputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }],
-        "name": "setVolume", "outputs": [{ "internalType": "bool", "name": "success", "type": "bool" }],
+        "inputs": [
+            { "internalType": "uint256", "name": "amount", "type": "uint256" }
+        ],
+        "name": "setVolume", "outputs": [
+            { "internalType": "bool", "name": "success", "type": "bool" }
+        ],
         "stateMutability": "nonpayable", "type": "function"
     },
 
     {
         "inputs": [],
-        "name": "totalInteractions", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "name": "totalInteractions", "outputs": [
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
         "stateMutability": "view", "type": "function"
     },
 
     {
-        "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }],
+        "inputs": [
+            { "internalType": "address", "name": "newOwner", "type": "address" }
+        ],
         "name": "transferOwnership", "outputs": [],
         "stateMutability": "nonpayable", "type": "function"
     },
 
     {
-        "inputs": [{ "internalType": "address", "name": "", "type": "address" }],
-        "name": "volume", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "inputs": [
+            { "internalType": "address", "name": "", "type": "address" }
+        ],
+        "name": "volume", "outputs": [
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
         "stateMutability": "view", "type": "function"
     },
 
     {
         "inputs": [],
-        "name": "wallets", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }],
+        "name": "wallets", "outputs": [
+            { "internalType": "uint256", "name": "", "type": "uint256" }
+        ],
         "stateMutability": "view", "type": "function"
     }
 
