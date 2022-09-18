386f49d68c347a00af1e155de3c8f388fcad7bb8
Yasmin Seidel <tiny@puddy.club>
2022-09-18 20:13:03 -0300

    fix

--- docs/crypto/configConvert.js
+++ docs/crypto/configConvert.js
@@ -1,60 +1,14 @@
-// Prepare Functions
-/* storyCfg.web3.abi.functionsString = [];
-for (const item in storyCfg.web3.abi.base) {
-
-    // Function
-    if (storyCfg.web3.abi.base[item].type === 'function') {
-
-        // View
-        if (storyCfg.web3.abi.base[item].stateMutability === 'view') {
-
-            let values = '';
-            let args = '';
-            for (const item2 in storyCfg.web3.abi.base[item].outputs) {
-
-                if (values) { values += ','; }
-                values += storyCfg.web3.abi.base[item].outputs[item2].type;
-
-            }
-
-            for (const item2 in storyCfg.web3.abi.base[item].inputs) {
-                
-                let extra = '';
-                if (args) { args += ','; }
-
-                if(storyCfg.web3.abi.base[item].inputs[item2].type === 'string') {
-                    extra += ' memory';
-                }
-
-                if(storyCfg.web3.abi.base[item].inputs[item2].name){
-                    extra += ' ';
-                }
-
-                args += `${storyCfg.web3.abi.base[item].inputs[item2].type}${extra}${storyCfg.web3.abi.base[item].inputs[item2].name}`;
-
-            }
-
-            storyCfg.web3.abi.functionsString.push('function ' + storyCfg.web3.abi.base[item].name + '(' + args + ') public view returns (' + values + ')');
-
-        }
-
-    }
-
-} */
-
 // Connection Update
-puddyWeb3.waitReadyProvider().then(() => {
-    puddyWeb3.waitAddress().then(async () => {
-        
-        await puddyWeb3.requestAccounts();
-        storyCfg.web3.contract = new ethers.Contract(
-            storyCfg.web3.contractAddress, 
-            /* storyCfg.web3.abi.functionsString,  */
-            storyCfg.web3.abi.base,
-            puddyWeb3.getSigner()
-        );
-        
-        return;
+puddyWeb3.on('signerUpdated', function (signer) {
 
-    });
+    storyCfg.web3.contract = new ethers.Contract(
+        storyCfg.web3.contractAddress,
+        storyCfg.web3.abi.base,
+        signer
+    );
+
+});
+
+puddyWeb3.waitAddress().then(() => {
+    puddyWeb3.requestAccounts();
 });
