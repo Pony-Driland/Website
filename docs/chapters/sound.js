4ba2e1229e8ba76420e4c2bc73d0937b1cf3997d
Yasmin Seidel <tiny@puddy.club>
2022-10-06 02:18:09 -0300

    new sound effect

--- docs/chapters/sound.js
+++ docs/chapters/sound.js
@@ -276,5 +276,6 @@ storyCfg.playlist = {
 storyCfg.sfx = {
     'heavy-rain-little-thunder': { type: 'file', module: 'main', value: '/audio/jasmindreasond/rain/heavy-rain-little-thunder.mp4' },
     'heavy-rain': { type: 'file', module: 'main', value: '/audio/jasmindreasond/rain/heavy-rain.mp4' },
-    'ambulance-alarm': { type: 'file', module: 'main', value: '/audio/jasmindreasond/alarm/ambulance.wav' }
+    'ambulance-alarm': { type: 'file', module: 'main', value: '/audio/jasmindreasond/alarm/ambulance.wav' },
+    'train-1': { type: 'file', module: 'main', value: '/audio/jasmindreasond/train/1.mp3' }
 };
