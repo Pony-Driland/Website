12def0e82f3a1328f23721253ea0f04bdc7418e7
Yasmin Seidel <tiny@puddy.club>
2022-10-17 17:10:39 -0300

    base64

---
+++ build/base64/index.js
@@ -0,0 +1,22 @@
+// Module
+const fs = require('fs');
+const path = require('path');
+const folder = path.join(__dirname, './');
+
+// Read Data
+fs.readdirSync(folder).forEach(file => {
+    if(file.endsWith('.jpg') || file.endsWith('.png') || file.endsWith('.gif') || file.endsWith('.webp') || file.endsWith('.webm')) {
+
+        // Prepare File
+        const filePath = path.join(folder, `./${file}`);
+        console.log(`Loading file ${filePath}`);
+
+        // Get Content
+        const content = fs.readFileSync(filePath, { encoding: 'base64' });
+        fs.writeFileSync(path.parse(filePath).name, content);
+
+    }
+});
+
+// Complete
+console.log('Tiny Complete! :3');
